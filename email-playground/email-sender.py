289099f43f509ee7ee9cdc7d01a5d1eb81e6ee7d
Rafah <irafah.alves@gmail.com>
2024-07-24 21:01:00 -0300

    PX-04: Send dynamic email

--- email-playground/email-sender.py
+++ email-playground/email-sender.py
@@ -1,13 +1,17 @@
 import smtplib
 
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html = Template(Path('index.html').read_text())
 
 email = EmailMessage()
 email['from'] = 'Joe Doe'
-email['to'] = 'joe-doe@hotmail.com'
+email['to'] = 'joe-doe@gmail.com'
 email['subject'] = 'Python Fun. Sending emails via Python!'
 
-email.set_content('I am a Python Master!')
+email.set_content(html.substitute(name='Tin Tin'), 'html')
 
 with smtplib.SMTP(host='smtp.office365.com', port=587) as smtp:
     smtp.ehlo()
